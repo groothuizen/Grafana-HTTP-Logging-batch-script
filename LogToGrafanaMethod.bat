2f0a4b5c834447cb8829459c988ca4de411d9b08
Jordan Groothuizen <jordanmaarten@gmail.com>
2025-10-29 15:13:07 +0100

    small comment edit

--- LogToGrafanaMethod.bat
+++ LogToGrafanaMethod.bat
@@ -1,6 +1,6 @@
 rem Param 1: (string) Log Level -required-
 rem Param 2: (string) Log Message -required-
-rem IMPORTANT when inserting double quotes, escape the character three times, example: \\\"
+rem IMPORTANT when inserting double quotes, escape the double quotes and the backslash like so: \\\"
 :LogToGrafana
 setlocal
     rem Get the current time in unix time and format it to a number that Grafana Loki can read:
@@ -49,4 +49,5 @@ setlocal enableextensions
     set /a z=(14 - 100%Month% %% 100) / 12, y= 10000%Year% %% 10000 - z
     set /a ut=y * 365 + y / 4 - y / 100 + y / 400 + (153 * (100%Month% %% 100 + 12 * z - 3) + 2) / 5 + Day - 719469
     set /a ut=ut * 86400 + 100%Hour% %% 100 * 3600 + 100%Minute% %% 100 * 60 + 100%Second% %% 100
+
 endlocal & set "%1=%ut%" & exit /B
